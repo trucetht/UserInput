8890c5bf324f80c49061d6d39032ffa87fa33dba
Trevor Hanson <52029257+trucetht@users.noreply.github.com>
2020-07-11 18:32:58 -0400

    Add files via upload

---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // asking for input for breakfast food
+            Console.WriteLine("What did you eat for breakfast?");
+            string breakfastFood = Console.ReadLine();
+
+            // asking for input for breakfast drink
+            Console.WriteLine("What did you drink for breakfast?");
+            string breakfastDrink = Console.ReadLine();
+
+            // asking for input for lunch food
+            Console.WriteLine("What did you eat for lunch?");
+            string lunchFood = Console.ReadLine();
+
+            // asking for input for lunch drink
+            Console.WriteLine("What did you drink for lunch?");
+            string lunchDrink = Console.ReadLine();
+
+            // asking for input for supper food
+            Console.WriteLine("What did you eat for supper?");
+            string supperFood = Console.ReadLine();
+
+            // asking for input for supper drink
+            Console.WriteLine("What did you drink for supper?");
+            string supperDrink = Console.ReadLine();
+
+            Console.WriteLine($"I ate {breakfastFood} and drank {breakfastDrink} for breakfast.");
+            Console.WriteLine($"I ate {lunchFood} and drank {lunchDrink} for lunch.");
+            Console.WriteLine($"I ate {supperFood} and drank {supperDrink} for supper.");
+
+
+        }
+    }
+}
